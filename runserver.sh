d159a851dd2b4e9278eb3719642a845448ebd2a1
sadatyussuf <tommyleespartan0@gmail.com>
2024-12-26 18:01:53 +0000

    runserver file

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-current_dir="$(dirname "$(readlink -f "$0")")"
+# current_dir="$(dirname "$(readlink -f "$0")")"
 
-source "$current_dir/.venv/Scripts/activate"
+# source "$current_dir/.venv/Scripts/activate"
 
 python manage.py makemigrations && python manage.py migrate
 python manage.py runserver
