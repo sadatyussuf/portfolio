9813a3af7467f239c1a28b3eac773d03b5e7b1a8
sadatyussuf <tommyleespartan0@gmail.com>
2025-01-03 12:20:18 +0000

    home page view

--- apps/manager/views.py
+++ apps/manager/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 
 
 def homepage_view(request):
-    return render(request, "pages/teams.html")
+    return render(request, "pages/landing.html")
 
 
-def project_view(request):
-    return render(request, "pages/projects.html")
+# def project_view(request):
+#     return render(request, "pages/projects.html")
 
 
-def project_detail_view(request, uid):
+# def project_detail_view(request, uid):
 
-    return render(request, "pages/project-blog.html")
+#     return render(request, "pages/project-blog.html")
